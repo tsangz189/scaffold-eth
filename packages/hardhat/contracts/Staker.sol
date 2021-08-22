43ad215ff5b6d410b74dd7099a07c09382c4b927
sang.trinh <sang.trinh@mic.com.tw>
2021-08-22 23:11:12 +0800

    add solution for challenge

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -5,26 +5,50 @@ import "./ExampleExternalContract.sol"; //https://github.com/OpenZeppelin/openze
 
 contract Staker {
 
+  mapping(address =>  uint256) public balances;
+  uint256 public constant threshold = 1 ether;
+  uint256 public deadline = now + 1 days;
+  bool public openForWithdraw = false;
   ExampleExternalContract public exampleExternalContract;
 
   constructor(address exampleExternalContractAddress) public {
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
+  event Stake(address, uint256);
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
-
+  function stake() public payable {
+    require(now < deadline);
+    balances[msg.sender] += msg.value;
+    emit Stake(msg.sender, msg.value);
+  }
 
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
-
+  function execute() public {
+    require(now > deadline);
+    if(address(this).balance > threshold) {
+      exampleExternalContract.complete{value: address(this).balance}();
+    }
+    else {
+      openForWithdraw = true;
+    }
+  }
 
 
   // if the `threshold` was not met, allow everyone to call a `withdraw()` function
-
-
+  function withdraw(address payable pay) public {
+    require(openForWithdraw);
+    pay.transfer(balances[pay]);
+    balances[pay] = 0;
+  }
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
+  function timeLeft() public view returns (uint256) {
+    if(now >= deadline) {
+      return 0;
+    }
+    return (deadline - now);
+  }
 }
